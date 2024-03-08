2653d8ad82f74f4276e7b00d0ce82ab12c545560
Theo <Dtheo98@gmail.com>
2024-03-08 11:24:27 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/iframe-has-title */
 import './App.css';
 
 function App() {
-  const docs = [ // Remote file
-    { uri: require("./crpp.pptx") }, // Local File
-  ];
-  
+
   return (
     <div className="App">
       <iframe src="https://onedrive.live.com/embed?resid=C41F082B0864B6EB%2192721&amp;authkey=%21AHORxyWttWPCJ48&amp;em=2&amp;wdAr=1.7777777777777777" width="100% !important" height="100vh !important" frameborder="0">Ceci est un  <a target="_blank" href="https://office.com">Microsoft Office</a> incorporé, avec <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
